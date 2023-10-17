61ce8b4e011d5a618b8ebba9d9548434d681d080
Yashdeep Jain <yashuofficial7@gmail.com>
2023-10-17 21:24:22 +0530

    unique user id UUID impl

--- UserService/UserService/src/main/java/com/lcwy/user/service/entities/User.java
+++ UserService/UserService/src/main/java/com/lcwy/user/service/entities/User.java
@@ -4,7 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.*;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "micro_users")
 public class User {
--- UserService/UserService/src/main/java/com/lcwy/user/service/servies/impl/UserServiceImpl.java
+++ UserService/UserService/src/main/java/com/lcwy/user/service/servies/impl/UserServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -17,6 +18,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User saveUser(User user) {
+        String randomUserId = UUID.randomUUID().toString();
+        user.setUserId(randomUserId);
         return userRepository.save(user);
     }
 
