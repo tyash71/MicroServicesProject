f84ee1e4f80131f4da644badd428d5955d894647
Yashdeep Jain <yashuofficial7@gmail.com>
2023-11-01 17:58:50 +0530

    hotelId randomUUID change

--- HotelService/HotelService/src/main/java/com/lcwy/hotel/services/impl/HotelServiceImpl.java
+++ HotelService/HotelService/src/main/java/com/lcwy/hotel/services/impl/HotelServiceImpl.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
+
 @Service
 public class HotelServiceImpl implements HotelService {
 
@@ -16,6 +18,8 @@ public class HotelServiceImpl implements HotelService {
 
     @Override
     public Hotel create(Hotel hotel) {
+        String hotelId = UUID.randomUUID().toString();
+        hotel.setId(hotelId);
         return hotelRepository.save(hotel);
     }
 
