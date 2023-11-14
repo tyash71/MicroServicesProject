8859c8f3d740c10496162a172c1f7f2bad509b30
Yashdeep Jain <yashuofficial7@gmail.com>
2023-11-14 16:20:04 +0530

    communication between user and rating service

--- UserService/UserService/src/main/java/com/lcwy/user/service/servies/impl/UserServiceImpl.java
+++ UserService/UserService/src/main/java/com/lcwy/user/service/servies/impl/UserServiceImpl.java
@@ -4,9 +4,13 @@ import com.lcwy.user.service.entities.User;
 import com.lcwy.user.service.exceptions.ResourceNotFoundException;
 import com.lcwy.user.service.repositories.UserRepository;
 import com.lcwy.user.service.servies.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,6 +19,11 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Override
     public User saveUser(User user) {
@@ -30,6 +39,12 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String userId) {
-        return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id: " + userId + " is not found on server."));
+        //get user from database with help of repository
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id: " + userId + " is not found on server."));
+        //fetch rating of above user from RATING SERVICE
+        //http://localhost:8083/ratings/users/07d1cb09-3fb0-4429-b983-1e95529e86e9
+        ArrayList forObject = restTemplate.getForObject("http://localhost:8083/ratings/users/"+user.getUserId(), ArrayList.class);
+        logger.info("{}",forObject);
+        return user;
     }
 }
