eaa68505a74fb7e46373eae4dfd5ec9b147cee7b
Yashdeep Jain <yashuofficial7@gmail.com>
2023-11-26 20:54:44 +0530

    Feign Client Post Mapping implementation

---
+++ UserService/UserService/src/main/java/com/lcwy/user/service/external/services/RatingService.java
@@ -0,0 +1,29 @@
+package com.lcwy.user.service.external.services;
+
+import com.lcwy.user.service.entities.Rating;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import java.util.Map;
+import java.util.Objects;
+
+@Service
+@FeignClient(name = "RATING-SERVICE")
+public interface RatingService {
+    //get
+
+    //post
+    @PostMapping("/ratings")
+    public ResponseEntity<Rating> createRating(Rating values);
+    //put
+    @PutMapping("/ratings/{ratingId}")
+    public ResponseEntity<Rating> updateRating(@PathVariable("ratingId") String ratingId, Rating rating);
+
+    @DeleteMapping("/ratings/{ratingId}")
+    public void deleteRating(@PathVariable String ratingId);
+}
--- UserService/UserService/src/test/java/com/lcwy/user/service/UserServiceApplicationTests.java
+++ UserService/UserService/src/test/java/com/lcwy/user/service/UserServiceApplicationTests.java
@@ -1,6 +1,9 @@
 package com.lcwy.user.service;
 
+import com.lcwy.user.service.entities.Rating;
+import com.lcwy.user.service.external.services.RatingService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -10,4 +13,13 @@ class UserServiceApplicationTests {
 	void contextLoads() {
 	}
 
+	@Autowired
+	private RatingService ratingService;
+//	@Test
+//	void createRating() {
+//		Rating rating = Rating.builder().rating(10).userId("").hotelId("").feedback("This is created using feign Client").build();
+//		Rating savedRating = ratingService.createRating(rating);
+//
+//		System.out.println("new rating created");
+//	}
 }
